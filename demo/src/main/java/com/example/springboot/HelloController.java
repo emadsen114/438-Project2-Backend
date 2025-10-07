c2e392e0de18ae3edda66e2b43a86f31beed9e44
Emily <emily.madsen03@gmail.com>
2025-10-07 10:54:55 -0700

    Set up Spring boot for our backend repo, its ready ti be worked with

---
+++ demo/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/second")
+	public String second() {
+		return "Greetings from second page!";
+	}
+
+}
