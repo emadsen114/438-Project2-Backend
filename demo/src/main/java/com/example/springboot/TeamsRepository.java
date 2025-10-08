6b17728bd9718d45b8e76a007bb6cce0bc099087
Jacob Zamora <113962665+JZmor@users.noreply.github.com>
2025-10-07 17:26:56 -0700

    it wasnt running so I added some stuff and now it is

--- demo/src/main/java/com/example/springboot/TeamsRepository.java
+++ demo/src/main/java/com/example/springboot/TeamsRepository.java
@@ -1,9 +1,9 @@
 package com.example.springboot;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.example.springboot.entities.Team;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {
+public interface TeamsRepository extends CrudRepository<Team, Long> {
 
 }
